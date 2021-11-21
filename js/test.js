17b5af0cb25f965ac2e28bea04f0ca4ead2c7c40
weenus <94806113+amee-racle@users.noreply.github.com>
2021-11-21 21:36:20 +0000

    jgjgfjjjuhj

---
+++ js/test.js
@@ -0,0 +1,23 @@
+let spr;
+function setup() {
+  createCanvas(400, 400);
+  spr = createSprite(width/2, height/2,
+    40, 40);
+  spr.shapeColor = color(255);
+  spr.velocity.y = 0;
+}
+function draw() {
+  background(50);
+  if (spr.position.y >= height) {
+    spr.velocity.y *= -1;
+    // set to height to prevent "tunneling"
+    spr.position.y = height;
+  }
+  // constant downward speed
+  // (i.e., gravity)
+  spr.addSpeed(0.25, 90);
+  drawSprites();
+}
+function mousePressed() {
+  spr.position.y = mouseY;
+}
