e7ef8fd0e62b343bc9a6de23cb8c13ad79e223ff
weenus <94806113+amee-racle@users.noreply.github.com>
2022-01-04 22:55:00 +0000

    stuff

--- tests/test3.js
+++ tests/test3.js
@@ -1,13 +1,15 @@
-let spr;
 function setup() {
-	createCanvas(400,400);
-	spr = createSprite(
-	  width/2, height/2, 40, 40);
-	spr.shapeColor = color(255);
-}
-function draw() {
-	background(50);
-	spr.position.x = mouseX
-	spr.position.y = mouseY
+    createCanvas(600,600);
 }
 
+function draw() {
+    background(32); // 50
+    let button = createImg("img/playbutton.png", "Play!");
+    button.position(250, 250);
+    button.mousePressed(testu);
+    button.size(100, 100);
+}
+
+function testu() {
+    alert("weenus");
+}
