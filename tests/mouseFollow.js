d360a4613945299b771f2e031fb33e8c4a77a8f2
weenus <94806113+amee-racle@users.noreply.github.com>
2022-01-09 22:26:25 +0000

    rddg

--- tests/mouseFollow.js
+++ tests/mouseFollow.js
@@ -1,13 +1,13 @@
 let spr;
 function setup() {
-  createCanvas(600, 600);
-  spr = createSprite(
-    width/2, height/2, 40, 40);
-  spr.shapeColor = color(255);
+    createCanvas(600, 600);
+    spr = createSprite(
+        width / 2, height / 2, 40, 40);
+    spr.shapeColor = color(255);
 }
 function draw() {
-  background(50);
-  spr.position.x = mouseX;
-  spr.position.y = mouseY;
-  drawSprites();
+    background(50);
+    spr.position.x = mouseX;
+    spr.position.y = mouseY;
+    drawSprites();
 }
